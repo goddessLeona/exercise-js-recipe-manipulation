c0b50b8ba14a376166ae81cc25018ebc56bad2c9
unknown <petra.j-son@outlook.com>
2024-11-17 12:04:07 +0100

    Upploaded all file for the exercise

---
+++ exercise-js-recipe-manipulation-main/index.js
@@ -0,0 +1,8 @@
+
+// Uppgift 1 : What is the name of the recipe?
+
+const recipeName = document.querySelector("h1");
+const nameCake = recipeName.innerText;
+console.log(`The name of the recipie is: ${nameCake}`)
+
+// Uppgift 2. What HTML tag is used to display the Recipe name?
