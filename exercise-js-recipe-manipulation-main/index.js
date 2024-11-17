8cbbf71be9f4f754df736f7229d167a55e9834ea
unknown <petra.j-son@outlook.com>
2024-11-17 17:00:42 +0100

    Finished until uppgift 8

--- exercise-js-recipe-manipulation-main/index.js
+++ exercise-js-recipe-manipulation-main/index.js
@@ -19,3 +19,55 @@ const fontSize = atributes.fontSize;
 console.log(`The font size on the P with class name description is: ${fontSize}`)
 
 // Uppgift 4. What is the value of the alt atrribute on the image?
+
+const image = document.querySelector("img");
+const altTag = image.getAttribute("alt");
+console.log(`The value on the alt atribute on the image is: ${altTag}`);
+
+// Uppgift 5: What is the dimensions and the url of the image? 
+
+const width = image.scrollWidth
+const hight = image.scrollHeight
+
+console.log(`The dementions of the iamge is, Hight: ${hight} pixels and Width ${width} pixels`)
+
+// Create an object that looks like this, and log it to the console:
+
+/*{
+  url: string
+  height: number,
+  width: number,
+}*/
+
+const topping = {
+    url: "string",
+    height: 13,
+    width: 15,
+}
+
+console.log(topping);
+
+// Uppgift 6: How many ingredients has the paste?
+
+const allIngridiens = document.querySelector(".ingredients-list-bottom");
+const nringribotton = allIngridiens.childElementCount;
+
+const allIngridiens2 = document.querySelector(".ingredients-list-paste");
+const nringridow = allIngridiens2.childElementCount;
+
+console.log("There are", nringribotton + nringridow , "ingridents for the paste");
+
+// Uppgift 7: Which is the forth element in the list containing the ingredients for the paste?
+
+const allLiTags = document.querySelectorAll("li");
+const ingr4 = allLiTags[3];
+const ing4text = ingr4.innerText;
+console.log(ing4text)
+
+// Uppgift 8: Create an an array of objects from the instructions. 
+// Each element in the array should be an object that looks like this:
+
+/*{
+  order: number;
+  text: instruction;
+}*/
